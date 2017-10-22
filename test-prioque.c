217fae56a051d37525a2d49a7ccca59f6b35dcd1
Jun Lin <junlin0604@gmail.com>
2017-10-22 00:31:59 -0500

    input function

--- test-prioque.c
+++ test-prioque.c
@@ -22,68 +22,30 @@ typedef struct Process_Data {
     int Res_time;
 } Process_Data;
 
-
-// comparison function for queue of Process_Data elements
-int some_type_compare(void *e1, void *e2) {
-  Process_Data *s1=(Process_Data *)e1;
-  Process_Data *s2=(Process_Data *)e2;
-  if (s1->a == s2->a && s1->buf != s2->buf) {
-    return 1;
-  }
-  else {
-    return 0;
-  }
+Process_Data *printProcess;
+void printPro(Queue *que){
+  printProcess = (Process_Data *)pointer_to_current(que);
+  printf("%d\n", printProcess->Pid);
 }
 
+int main() {
+  Queue hq;     //highest priority
+  Queue mq;     //medium priority
+  Queue lq;     //lowest priority
 
-int main(int argc, char *argv[]) {
-  FILE* fptr;
-  fptr = fopen("input.txt","r");
-  Queue q;
-  init_queue(&q, sizeof(Process_Data), TRUE, some_type_compare, FALSE);
-  Process_Data time, pid, run, io, repeat;
-  int clock = 0;
-  time.a = 0;
-  while(!empty_queue(&q)){
-    for (int i = 1; i < repeat.a ; ++i) {
+  //initialize queue
+  init_queue(&hq, sizeof(Process_Data), TRUE, NULL, TRUE);
+  init_queue(&mq, sizeof(Process_Data), TRUE, NULL, TRUE);
+  init_queue(&lq, sizeof(Process_Data), TRUE, NULL, TRUE);
 
-    }
-  }
-  fscanf(fptr,"%d", &time);
+  Process_Data temp;
+  Process_Data
 
 
-  add_to_queue(&q, &s1, s1.a+1);
-  add_to_queue(&q, &s2, s2.a+1);
-  add_to_queue(&q, &s3, s3.a+1);
-  add_to_queue(&q, &s4, s4.a+1);
-  rewind_queue(&q);
-  printf("Queue contains:\n");
-  int clock = 0;
-  while (! end_of_queue(&q)) {
-    printf("%d %s with priority %d\n",
-           ((Process_Data *)pointer_to_current(&q))->a,
-           ((Process_Data *)pointer_to_current(&q))->buf,
-           current_priority(&q));
-    next_element(&q);
-  }
-  remove_from_front(&q, &e);
-  printf("Removed %d %s from queue.\n", e.a, e.buf);
-  printf("Queue contains:\n");
-  while (! end_of_queue(&q)) {
-    printf("%d %s with priority %d\n",
-           ((Process_Data *)pointer_to_current(&q))->a,
-           ((Process_Data *)pointer_to_current(&q))->buf,
-           current_priority(&q));
-    next_element(&q);
-  }
-  printf("Destroying queue.\n");
-  destroy_queue(&q);
-  printf("Queue contains:\n");
-  while (! end_of_queue(&q)) {
-    printf("%d %s with priority %d\n",
-           ((Process_Data *)pointer_to_current(&q))->a,
-           ((Process_Data *)pointer_to_current(&q))->buf,
-           current_priority(&q));
-    next_element(&q);
+  int temp_time, temp_pid, temp_run, temp_io, temp_rep;
+
+  while (scanf("%d", &temp_time) != EOF){
+    scanf("%d", &temp_pid);
+
   }
 }
